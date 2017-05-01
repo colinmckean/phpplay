f968f180dd689900ca95eea1f9583e55188616f8
Colin McKean <ezikill@gmail.com>
2017-05-01 20:24:42 +0000

    more variables

--- hello-world.php
+++ hello-world.php
@@ -1,9 +1,33 @@
-<html><body>
 <?php
-echo 'Hello world from Cloud9!<br>';
+define("TITLE", "Learning about PHP variables and constants");
 ?>
-<?php
-$name = "Colin";
-echo "Hello " . $name ."<br>";
-?></body>
+<html>
+<head>
+    <title><?php echo TITLE; ?></title>
+</head>
+<body>
+	<?php
+	echo 'Hello world from Cloud9!<br>';
+	?>
+	<?php
+	$name = "Colin";
+	echo "Hello " . $name ."<br>";
+	?>
+	<?php
+//some typical variables
+//BOOLEAN
+	$hungry = true;
+//INTEGER
+	$myHungerLevel = 100;
+//FLOAT
+	$percentageOfFoodAte = 99.7;
+//STRING
+	$fullName = "Colin McKean";
+	echo "Hello my name is $fullName I have ate $percentageOfFoodAte% of the food in my cupboards.";
+// CONSTANT
+// see title for an example
+	define("TITLE", "Learning about PHP variables and constants");
+	?>
+	
+</body>
 </html>
