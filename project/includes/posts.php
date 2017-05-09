5b6c60fa34b6b826133108c896fae22636dcbc54
Colin McKean <ezikill@gmail.com>
2017-05-09 21:06:06 +0000

    move posts to external static posts.php file

---
+++ project/includes/posts.php
@@ -0,0 +1,25 @@
+<?php 
+    $posts = array(
+        array(
+            "post-title" =>  "Sample blog post",
+            "post-date" => "January 1, 2017",
+            "post-author" => "Mark",
+            "post-content" => "<p>Shoulder pastrami capicola short loin pork loin picanha pig tenderloin. Picanha swine pork loin, porchetta leberkas pork belly doner venison jowl short loin strip steak salami chuck alcatra. Filet mignon tongue swine, turducken beef ribs strip steak sirloin alcatra. Doner strip steak swine frankfurter prosciutto venison turducken.</p><p>Strip steak cow pork andouille boudin pork belly filet mignon brisket shank pork chop kielbasa hamburger pig. Kevin shoulder tenderloin cow chuck. Jerky turkey tenderloin, ham ball tip meatball landjaeger. Turkey beef ribs meatloaf chuck ham. Tongue pastrami kielbasa jerky leberkas.</p><p>Meatloaf tri-tip salami, kielbasa filet mignon hamburger cupim drumstick picanha pork loin leberkas pork venison porchetta beef. Cow pork t-bone, spare ribs sirloin tenderloin burgdoggen pork chop beef biltong turducken ribeye. Turducken sirloin meatball pork chop shankle sausage. Boudin t-bone shankle tenderloin turducken, shoulder flank bresaola drumstick pork loin.</p>"
+            
+            ),
+                    array(
+            "post-title" =>  "Another blog post",
+            "post-date" => "February 1, 2017",
+            "post-author" => "Jacob",
+            "post-content" => "<p>Kielbasa ground round meatball pancetta drumstick swine. Pork filet mignon tongue drumstick cow burgdoggen brisket, turducken venison sirloin shank pancetta pork loin jerky. Filet mignon turducken tongue shoulder, pancetta shankle prosciutto venison chuck. Beef pig boudin ham hock turducken biltong rump. Filet mignon shank biltong boudin ribeye ground round rump short ribs short loin hamburger tri-tip pig burgdoggen meatball ham hock. Hamburger frankfurter ball tip, cupim shank prosciutto t-bone.</p><p>Strip steak cow pork andouille boudin pork belly filet mignon brisket shank pork chop kielbasa hamburger pig. Kevin shoulder tenderloin cow chuck. Jerky turkey tenderloin, ham ball tip meatball landjaeger. Turkey beef ribs meatloaf chuck ham. Tongue pastrami kielbasa jerky leberkas.</p><p>Meatloaf tri-tip salami, kielbasa filet mignon hamburger cupim drumstick picanha pork loin leberkas pork venison porchetta beef. Cow pork t-bone, spare ribs sirloin tenderloin burgdoggen pork chop beef biltong turducken ribeye. Turducken sirloin meatball pork chop shankle sausage. Boudin t-bone shankle tenderloin turducken, shoulder flank bresaola drumstick pork loin.</p>"
+            
+            ),
+                    array(
+            "post-title" =>  "New feature",
+            "post-date" => "March 1, 2017",
+            "post-author" => "Chris",
+            "post-content" => "<p>Frankfurter t-bone landjaeger boudin filet mignon picanha doner andouille. Flank drumstick pork belly, sirloin chicken pork loin tenderloin andouille burgdoggen bresaola. T-bone drumstick short ribs chicken pig picanha. Cupim shoulder meatball burgdoggen chuck.</p><p>Meatloaf tri-tip salami, kielbasa filet mignon hamburger cupim drumstick picanha pork loin leberkas pork venison porchetta beef. Cow pork t-bone, spare ribs sirloin tenderloin burgdoggen pork chop beef biltong turducken ribeye. Turducken sirloin meatball pork chop shankle sausage. Boudin t-bone shankle tenderloin turducken, shoulder flank bresaola drumstick pork loin.</p>"
+            
+            )
+        )
+?>
